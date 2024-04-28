fa2ac4a04bda8c7eea31ebec0ab00515ee5f24a6
Caio Santos <caio.santos687@gmail.com>
2024-04-27 23:18:43 -0300

    init project

---
+++ test/mba_fiap/preparo_service_test.clj
@@ -0,0 +1,7 @@
+(ns mba-fiap.preparo-service-test
+  (:require [clojure.test :refer :all]
+            [mba-fiap.preparo-service :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
