239b51b2c4886af8f4dd132e134ee32544ced283
Caio Santos <caio.santos687@gmail.com>
2024-05-18 22:36:41 -0300

    adjust github action

--- build.clj
+++ build.clj
@@ -1,8 +1,8 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require
-    [clojure.tools.build.api :as b]
-    [clojure.tools.deps :as t]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.tools.deps :as t]
+            [clojure.tools.build.api :as b]))
 
 (def lib 'net.clojars.mba-fiap/preparo-service)
 (def version "0.1.0-SNAPSHOT")
@@ -10,42 +10,44 @@
 (def class-dir "target/classes")
 
 (defn test
-  "Run all the tests."
-  [opts]
-  (println "\nRunning tests...")
-  (let [basis (b/create-basis {:aliases [:test]})
-        combined (t/combine-aliases basis [:test])
-        cmds (b/java-command
-               {:basis     basis
-                :java-opts (:jvm-opts combined)
-                :main      'clojure.main
-                :main-args ["-m" "cognitect.test-runner"]})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
-  opts)
+      "Run all the tests."
+      [opts]
+      (println "\nRunning tests...")
+      (let [basis    (b/create-basis {:aliases [:test]})
+            combined (t/combine-aliases basis [:test])
+            cmds     (b/java-command
+                       {:basis basis
+                        :java-opts (:jvm-opts combined)
+                        :main      'clojure.main
+                        :main-args ["-m"
+                                    "cloverage.coverage"
+                                    "--test-ns-path" "test"
+                                    "--src-ns-path" "src"
+                                    "--runner" "eftest"]})
+            {:keys [exit]} (b/process cmds)]
+           (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
+      opts)
 
-(defn- uber-opts
-  [opts]
-  (assoc opts
-    :lib lib :main main
-    :uber-file (format "target/%s-%s.jar" lib version)
-    :basis (b/create-basis {})
-    :class-dir class-dir
-    :src-dirs ["src"]
-    :ns-compile [main]))
+(defn- uber-opts [opts]
+       (assoc opts
+              :lib lib :main main
+              :uber-file (format "target/%s-%s.jar" lib version)
+              :basis (b/create-basis {})
+              :class-dir class-dir
+              :src-dirs ["src"]
+              :ns-compile [main]))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)."
-  [opts]
-  (test opts)
-  (b/delete {:path "target"})
-  (let [opts (uber-opts opts)]
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
-    (println (str "\nCompiling " main "..."))
-    (b/compile-clj opts)
-    (println "\nBuilding JAR...")
-    (b/uber opts))
-  opts)
+(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
 
-
-(println "Classpath:" (System/getProperty "java.class.path"))
+      (b/delete {:path "target"})
+      (let [opts (uber-opts opts)]
+           (println "\nCopying source...")
+           (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+           (println (str "\nCompiling " main "..."))
+           (b/compile-clj opts)
+           (println "\nBuilding JAR...")
+           (b/uber opts)
+           (when (:bdd opts)
+                 (println "\nRunning Cucumber tests...\n"
+                          (sh/sh "clojure" "-M:test:cucumber" "-g" "./test/mba_fiap/" "./test/resources/"))))
+      opts)
